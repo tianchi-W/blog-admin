c7630062dac807e501c929eb3942def84e14772b
wei <weitianchi001@163.com>
2023-04-29 23:36:19 +0900

    refactor 配置环境变量 跨域前端

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,45 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
+console.log(process.env.NODE_ENV)
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default () => {
+  return defineConfig({
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '') // 不可以省略rewrite
+        }
+      }
+      // proxy: {
+      //   [`${loadEnv(mode, process.cwd()).VITE_APP_BASE_API}`]: {
+      //     target: loadEnv(mode, process.cwd()).VITE_TEST_HOST, // 线上
+      //     // rewrite: (path:any) => path.replace(/^\/api/, ''),
+      //     changeOrigin: true,
+      //     ws: true
+      //   }
+      // }
     }
-  }
-})
+  })
+}
